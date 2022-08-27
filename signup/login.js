97320073f73aa96552b84befa1ddfab899764732
sanju682295 <sanju682295@gmail.com>
2022-08-27 22:44:37 +0530

    done

--- signup/login.js
+++ signup/login.js
@@ -17,13 +17,14 @@ function final(){
     if(saveddata[0].email===email1&&saveddata[0].password===pass1){
         alert("Login success");
         localStorage.setItem("name",JSON.stringify(saveddata[0].name))
+        window.location.href="../check/check.html"
     }else{
         alert("Either email or password is incorrect")
     }
    }
 }
 function entry(){
-    window.location.href="../check/check.html"
+  
 }
 document.getElementById("signup").addEventListener("click",function(){
     window.location.href="signup.html"
