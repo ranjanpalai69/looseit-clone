569cf2019a5e4481605e24e5f73bafda2942066c
Md Jawed <mdjawedh22@gmail.com>
2022-08-27 18:24:52 +0530

    done

---
+++ landing page2.2/landing.js
@@ -0,0 +1,34 @@
+var left=1;
+var right=3
+function show(){
+  for(i=left; i<=right; i++){ 
+  document.getElementById("c"+i).style.display="inline-block"
+}
+}
+function moveLeft(){
+if(left<=3&&right<=7){
+document.getElementById('c'+left).style.display="none";
+left+=1;
+right+=1;
+
+for(i=left; i<=right; i++){ 
+  document.getElementById("c"+i).style.display="inline-block"
+}
+}
+else
+   return
+}
+
+function moveRight(){
+if(left>=2&&right>=3){
+document.getElementById('c'+right).style.display="none";
+left-=1;
+right-=1;
+
+for(i=left; i<=right; i++){ 
+  document.getElementById("c"+i).style.display="inline-block"
+}
+}
+else
+   return
+}
